e9b0e97628674a1ddf327253e51424da6d6328d9
Ing-Estefania-Pachon <ing.estefaniapachon@gmail.com>
2025-07-18 16:46:56 -0500

    Se agrego la solución y es funcional, pero aun esta feito

---
+++ contador.js
@@ -0,0 +1,33 @@
+let contador = 0;
+
+let mostrarValor = document.getElementById("valor");
+
+// Función para sumar
+document.getElementById("sumar").addEventListener("click", function(){
+
+    contador ++;
+
+    mostrarValor.textContent = contador;
+
+    if (contador >= 10) {
+        mostrarValor.style.color = "purple";
+    } else {
+    mostrarValor.style.color = "black";
+    }
+    
+})
+
+// Función para restar
+document.getElementById("restar").addEventListener("click", function(){
+
+    contador --;
+
+    mostrarValor.textContent = contador;
+
+    if (contador >= 10) {
+        mostrarValor.style.color = "purple";
+    } else {
+    mostrarValor.style.color = "black";
+    }
+    
+})
